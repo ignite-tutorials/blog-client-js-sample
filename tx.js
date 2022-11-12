a5c05388ca4670d174496218568e5b62b1d9eb77
Bruce Li <115622965+arch-designer-cosmos@users.noreply.github.com>
2022-11-12 15:53:32 +0800

    Create tx.js

---
+++ tx.js
@@ -0,0 +1,41 @@
+import { Client } from 'blog-client-ts';
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { assertIsDeliverTxSuccess } from "@cosmjs/stargate";
+
+const mnemonic = "vast unlock traffic humor destroy crush decrease tornado heart color despair prize shell whip robust wheel magnet unusual suffer tent april identify creek outside";
+const prefix = "blog";
+
+const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+  mnemonic,{
+    prefix: prefix
+  }
+);
+
+const client = new Client({ 
+    apiURL: "http://localhost:1317",
+    rpcURL: "http://localhost:26657",
+    prefix: prefix
+  },wallet
+);
+
+const fee = {
+    amount: [
+      {
+        denom: "token",
+        amount: "5",
+      },
+    ],
+    gas: "180000",
+  };  
+
+const result = await client.BlogBlog.tx.sendMsgCreatePost({
+    value: {
+      creator:'blog1n9jv7mla2qtwqt2g2pukypnm7scf7vqexz8vv0',
+      title:'Hello ignite',
+      body:"I'm Bruce, how are you?"
+    }},
+    fee,
+    'New blog post about ignite'
+);
+
+assertIsDeliverTxSuccess(result);
